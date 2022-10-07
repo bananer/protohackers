2fe0bf79cdbb2c9bc41cc8c41be906cb35d8f876
Philip Nagler-Frank <philip@nagler.world>
2022-10-07 21:16:20 +0200

    unusual database: rough first implementation

---
+++ unusual_database/main.py
@@ -0,0 +1,31 @@
+import os
+import socket
+
+
+def start_db(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("0.0.0.0", port))
+    data = dict()
+    encoding = "utf-8"
+    while True:
+        raw_request, addr = sock.recvfrom(1000)
+        request = raw_request.decode(encoding)
+        eq_pos = request.find('=')
+        if eq_pos == -1:
+            # retrieve
+            key = request
+            val = data.get(key)
+            if val is None:
+                val = ""
+            response = key + '=' + val
+            sock.sendto(response.encode(encoding), addr)
+        else:
+            # insert
+            key = request[0:eq_pos]
+            value = request[eq_pos+1:]
+            data[key] = value
+
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT') or "2345"
+    start_db(int(port))
